a8658d20271e8bffbf6d9492828bb2917e94dec7
kalairethu <30317791+kalairethu@users.noreply.github.com>
2017-07-22 23:44:23 -0700

    Create pg9.c

---
+++ pg9.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int i,sum=0,num;
+printf("enter the number");
+scanf("%d",&num);
+for(i=1;i<=num;i++)
+{
+sum=sum+i;
+}
+printf("%d is the sum of N number");
+scanf("%d",&sum);
+}
