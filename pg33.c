1db7d92db77653c862d200b5a80b52c6be22ff0c
kalairethu <30317791+kalairethu@users.noreply.github.com>
2017-07-24 01:22:24 -0700

    Create pg33.c

---
+++ pg33.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str[100];
+int i=0,l=0,f=1;
+puts("enter the string\n");
+gets(str);
+for(i=0;str[i]!='\0';i++)
+{
+l=l+1;
+}
+
+printf("the number of words in the string are %d:,f);
+}
